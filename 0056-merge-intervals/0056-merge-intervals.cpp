fe1578b78558a604ceef8c6f0322ed4844dc9f4f
vskr-23 <vuradisaikiran2356@gmail.com>
2025-02-04 09:48:26 +0530

    Time: 3 ms (90.07%), Space: 23.6 MB (92.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
         vector<vector<int>>merged;
-        for(auto interval:intervals){
-            if(merged.empty() || merged.back()[1]<interval[0]){
-                merged.push_back(interval);
+        for(int i=0;i<intervals.size();i++){
+            if(merged.empty() || merged.back()[1]<intervals[i][0]){
+                merged.push_back(intervals[i]);
             }else{
-                merged.back()[1]=max(merged.back()[1],interval[1]);
+                merged.back()[1]=max(merged.back()[1],intervals[i][1]);
             }
         }
         return merged;
