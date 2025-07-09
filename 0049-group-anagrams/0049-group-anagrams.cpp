0e7427745ea4a01a3a9a986668636cc5ab85591a
vskr-23 <vuradisaikiran2356@gmail.com>
2025-07-09 10:03:10 +0530

    Time: 25 ms (27.75%), Space: 27.9 MB (22.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string>temporary=strs;
+        vector<string>copy=strs;
         unordered_map<string,vector<string>>mpp;
-        for(int i=0;i<temporary.size();i++){
-            sort(temporary[i].begin(),temporary[i].end());
-            mpp[temporary[i]].push_back(strs[i]);
+        for(int i=0;i<copy.size();i++){
+            sort(copy[i].begin(),copy[i].end());
+            mpp[copy[i]].push_back(strs[i]);
         }
         vector<vector<string>>ans;
         for(auto it:mpp){
-            ans.push_back(it.second);
+            vector<string>sub;
+            for(auto str:it.second){
+                sub.push_back(str);
+            }
+            ans.push_back(sub);
         }
         return ans;
     }
